c86d6da4536533e3f61cfc4dba14b86f8a31da15
Irina Dolgova <claycharm@yandex.ru>
2022-07-12 11:52:29 +0300

    Tests of class GameStore

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -15,5 +15,75 @@ public class GameStoreTest {
     }
 
     // другие ваши тесты
-    // test
+    @Test
+    public void shouldAddOnePlayer() {
+        GameStore store = new GameStore();
+        store.addPlayTime("Petr", 5);
+        String  expected = "Petr";
+        String actual = store.getMostPlayer();
+        assertEquals(expected, actual);
+
+ }
+    @Test
+    public void shouldAddTwoPlayerAndAndReturnBestPlayer() {
+        GameStore store = new GameStore();
+        store.addPlayTime("Petr", 5);
+        store.addPlayTime("Ivan", 1);
+        String expected = "Petr";
+        String actual = store.getMostPlayer();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAddThreePlayerAndReturnBestPlayer() {
+        GameStore store = new GameStore();
+        store.addPlayTime("Petr", 2);
+        store.addPlayTime("Igor", 5);
+        store.addPlayTime("Ivan", 5);
+        String expected = "Igor";
+        String actual = store.getMostPlayer();
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldReturnNull() {
+        GameStore store = new GameStore();
+
+        String expected = null;
+        String actual = store.getMostPlayer();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCheckBorderOfTime() {
+        GameStore store = new GameStore();
+        store.addPlayTime("Petr", -2);
+
+        String expected = null;
+        String actual = store.getMostPlayer();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGetSumPlayerTime() {
+        GameStore store = new GameStore();
+        store.addPlayTime("Petr", 2);
+        store.addPlayTime("Igor", 5);
+        store.addPlayTime("Ivan", 5);
+        int expected = 12;
+        int actual = store.getSumPlayedTime();
+        assertEquals(expected, actual);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
