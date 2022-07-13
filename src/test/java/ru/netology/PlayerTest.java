1033bf0c1b411f357d377b24373adf159022d6a8
Natalia <nika_pozitivka@mail.ru>
2022-07-14 00:00:28 +0300

    "player"

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -20,4 +20,83 @@ public class PlayerTest {
     }
 
     // другие ваши тесты
+
+    @Test
+    public void shouldPlay() { // Суммарное сыгранное время игроком, после установки игры.
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Нетология Баттл Онлайн2", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.installGame(game2);
+        player.play(game, 4);
+        player.play(game2, 1);
+
+        int expected = 5;
+        int actual = player.sumGenre(game.getGenre());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldPlay2() {//Игра не установлена.
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Нетология Баттл Онлайн2", "Аркады");
+
+        Player player = new Player("Petya");
+
+        player.play(game2, 5);
+
+        assertThrows(RuntimeException.class, () -> player.play(game2, 5));
+    }
+    @Test
+    public void shouldMostPlayerByGenre() { // Возвращает название игры, в которую играли больше всего.
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Нетология Баттл Онлайн2", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.installGame(game2);
+        player.play(game, 3);
+        player.play(game2, 1);
+
+        Game expected = player.mostPlayerByGenre("Аркады");
+        String actual = game.getTitle();
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldMostPlayerByGenre1() { // Возвращает название игры, в которую играли больше всего или null
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Нетология Баттл Онлайн2", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.installGame(game2);
+        player.play(game, 3);
+        player.play(game2, 1);
+
+        Game expected = player.mostPlayerByGenre("Танчики");
+        String actual = game.getTitle();
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldInstallGame() { //Возвращает суммарное время игры при повторной установки игры.
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.play(game, 4);
+        //player.installGame(game);
+
+        int expected = 4;
+        int actual = player.sumGenre("Аркады");
+
+        assertEquals(expected, actual);
+    }
 }
